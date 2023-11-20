f10d1da6df37b3114b9f417da0c5dab5da639359
Nathalia Cosim <nathalia.pcosim@gmail.com>
2023-11-20 15:04:00 -0400

    Updates...

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { ColaboradoresModule } from './colaboradores/colaboradores.module';
 import { AcoesModule } from './acoes/acoes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CorsMiddleware } from './cors.middleware';
+import * as dotenv from 'dotenv';
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://pixelsdecarinho:<senha>@pixels.rb1rza5.mongodb.net/pixels?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(`mongodb+srv://${dbUser}:${dbPassword}@pixels.rb1rza5.mongodb.net/pixels?retryWrites=true&w=majority`),
     EntradasModule,
     SaidasModule,
     ColaboradoresModule,
